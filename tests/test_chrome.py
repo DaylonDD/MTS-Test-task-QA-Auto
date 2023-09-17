d9c48babc21689749736eed8922a636733242185
DaylonDD <129903479+DaylonDD@users.noreply.github.com>
2023-09-17 18:53:52 +0400

    Add files via upload

---
+++ tests/test_chrome.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+
+service = Service(executable_path=ChromeDriverManager().install())
+browser = webdriver.Chrome(service=service)
+
+activation_button = "//mts-tariff-card[@id='4705081']//div[@class='btn universal-card-button'][contains(text(),'Подключить')]"
+send_appl = "//button[@class='btn btn_large ng-star-inserted']"
+success = "//div[@class='dialog-container ng-star-inserted']"
+spec_symbols = "!@#$%^&*()_+=-№[]';/.,\":\\|}{<>?\n"
+browser.implicitly_wait(3)
+
+browser.get("https://moskva.mts.ru/personal/mobilnaya-svyaz/tarifi/vse-tarifi/mobile-tv-inet")
+browser.find_element(By.XPATH, activation_button).click()
+browser.find_element(By.ID, 'username').send_keys(spec_symbols)
+browser.find_element(By.ID, 'phone').send_keys('0123456789')
+time.sleep(3)
+browser.find_element(By.XPATH, send_appl).click()
+
+browser.quit()
+
